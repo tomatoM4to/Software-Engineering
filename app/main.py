42a600476c5acd14b5d59fce270c1eed4112dfd7
tomatoM4to <tomatomato2121@gmail.com>
2026-05-05 18:18:49 +0900

    feature: access token 발급 함수 구현

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from core import kis_auth as ka
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("!!!fast api server start!!!")
+    ka.auth()
+    yield
+    print("!!!fast api server end!!!")
 
+app = FastAPI(
+    title="Trading Server",
+    lifespan=lifespan
+)
 
 @app.get("/")
 def read_root():
