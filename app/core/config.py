f58e0e02d96e9fd740db8fa95c71e038c5881289
tomatoM4to <tomatomato2121@gmail.com>
2026-05-12 20:47:06 +0900

    ruff

--- app/core/config.py
+++ app/core/config.py
@@ -10,9 +10,7 @@ class Settings(BaseSettings):
     ENABLE_SCHEDULER: bool = False
 
     model_config = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8",
-        extra="ignore"
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
 
 
